d1b8633a1157c511dd056e916fa611c3ef4d23ff
Truc <trucdg@bu.edu>
2022-06-09 03:47:07 +0000

    change server's response

--- portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
@@ -13,7 +13,7 @@ public class HelloWorldServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello Truc!</h1>");
+    response.getWriter().println("<h1>Hello my friend!</h1>");
   }
 }
 
